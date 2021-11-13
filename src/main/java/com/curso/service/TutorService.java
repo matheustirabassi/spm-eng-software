87daf94c20c362f3dbd360f4450f86563f7751ca
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2021-11-13 18:44:04 -0300

    feat: :sparkles: Criando a classe tutor
    Classe Tutor crianda com as suas respectivas classe DAO, Service e Controller(Bean)

---
+++ src/main/java/com/curso/service/TutorService.java
@@ -0,0 +1,42 @@
+package com.curso.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.curso.dao.TutorDAO;
+import com.curso.modelo.Tutor;
+import com.curso.util.NegocioException;
+
+public class TutorService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private TutorDAO tutorDAO;
+
+
+	public void salvar(Tutor tutor) throws NegocioException {		
+		this.tutorDAO.save(tutor);		
+	}	
+	
+	public List<Tutor> buscarTodos() {
+		return tutorDAO.findAll();
+	}
+	
+	public void deletar(Tutor tutor) throws NegocioException {
+		tutorDAO.delete(tutor);		
+	}
+
+	
+	public Tutor buscarPorEmail(String email) {
+		return tutorDAO.findByEmail(email);
+		
+	}
+	
+	public List<Tutor> buscarPorNome(String nome){
+		return tutorDAO.findByName(nome);
+	}
+	
+}
