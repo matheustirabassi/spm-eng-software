417c3bb34a790e485d9ab852c48ccdf41949f4a4
Matheus Tirabassi <64479165+matheustirabassi@users.noreply.github.com>
2021-11-14 22:06:04 -0300

    Revert "feat: :sparkles: Criando a classe tutor"
    This reverts commit 9f97462dc3eff5772055b0648ae935268daa3e24.

--- src/main/java/com/curso/modelo/enums/Sexo.java
+++ src/main/java/com/curso/modelo/enums/Sexo.java
@@ -2,16 +2,6 @@ package com.curso.modelo.enums;
 
 public enum Sexo {
 
-	FEMININO("feminino"),
-	MASCULINO("masculino");
-	
-	private String descricao;
-	
-	Sexo(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
+	FEMININO,
+	MASCULINO
 }
